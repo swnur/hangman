4acee762af7aa0c94d0ec9f71791b2273a0a9031
Nurbek Shukan <shukannurbek@gmail.com>
2024-11-20 10:20:21 +0100

    Move the scaffolds to separate enum class

---
+++ src/HangmanState.java
@@ -0,0 +1,70 @@
+public enum HangmanState {
+    ZERO("""
+                    +---+
+                    |   |
+                        |
+                        |
+                        |
+                        |
+                ==========="""
+    ),
+    ONE("""
+                +---+
+                |   |
+                O   |
+                    |
+                    |
+                    |
+            ==========="""
+    ),
+    TWO("""
+                +---+
+                |   |
+                O   |
+                |   |
+                    |
+                    |
+            ==========="""),
+    THREE("""
+                +---+
+                |   |
+                O   |
+               /|   |
+                    |
+                    |
+            ==========="""),
+    FOUR("""
+                +---+
+                |   |
+                O   |
+               /|\\  |
+                    |
+                    |
+            ==========="""),
+    FIVE("""
+                +---+
+                |   |
+                O   |
+               /|\\  |
+               /    |
+                    |
+            ==========="""),
+    SIX("""
+                +---+
+                |   |
+                O   |
+               /|\\  |
+               / \\  |
+                    |
+            ===========""");
+
+    private final String scaffold;
+
+    HangmanState(String scaffold) {
+        this.scaffold = scaffold;
+    }
+
+    public String getScaffold() {
+        return scaffold;
+    }
+}
