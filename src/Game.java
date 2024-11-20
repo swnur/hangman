7337e72d2deaa46cbc69f1054c6ce2a2bc3de0d8
Nurbek Shukan <shukannurbek@gmail.com>
2024-11-20 10:22:47 +0100

    Separate word service  methods to separate class, changes in Game class according to previous refactors

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,68 @@
-package PACKAGE_NAME;public class Game {
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Game {
+    private final GameRenderer renderer;
+    private final WordService wordService;
+
+    public Game() {
+        renderer = new GameRenderer();
+        wordService = new WordService();
+    }
+
+    public void start() {
+        System.out.println("\tWelcome to Hangman Game!");
+
+        do {
+            System.out.print("\nEnter [S] to start game or [E] to exit: ");
+            String input = wordService.getUserStartInput();
+
+            if (input.equals("e")) {
+                System.exit(0);
+            } else if (input.equals("s")) {
+                startRound();
+            } else {
+                System.out.println("Invalid input. Please try again to enter [S] to start or [E] to end.");
+            }
+        } while (true);
+    }
+
+    private void startRound() {
+        final int TOTAL_ATTEMPTS = 7;
+        String word = wordService.getRandomWord();
+        char[] guessWord = new char[word.length()];
+        Arrays.fill(guessWord, '_');
+
+        Set<Character> listOfErrors = new HashSet<>();
+        int countOfErrors = 0;
+
+        do {
+            renderer.print(guessWord, listOfErrors, countOfErrors, TOTAL_ATTEMPTS);
+
+            char inputCharacter = wordService.getUserGuessInput();
+            boolean isCorrectGuess = wordService.checkUserGuessInput(word, inputCharacter);
+
+            if (isCorrectGuess) {
+                wordService.replaceFoundCharacter(guessWord, word, inputCharacter);
+                if (word.equals(new String(guessWord))) {
+                    System.out.println("You won the game!!!");
+                    System.out.println("The correct word is " + word);
+                    return;
+                }
+            } else {
+                if (!listOfErrors.contains(inputCharacter)) {
+                    countOfErrors += 1;
+                    listOfErrors.add(inputCharacter);
+                }
+                if (countOfErrors == TOTAL_ATTEMPTS) {
+                    System.out.println("\nUnfortunately you lost the game!");
+                    System.out.println("Better luck next time!");
+                    System.out.println("The word was: " + word);
+                    return;
+                }
+            }
+        } while (true);
+    }
 }
---
+++ src/WordService.java
@@ -0,0 +1,55 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class WordService {
+    private final Scanner scanner;
+    private final Random random;
+
+    public WordService() {
+        random = new Random();
+        scanner = new Scanner(System.in);
+    }
+
+    public String getUserStartInput() {
+        return scanner.nextLine().toLowerCase();
+    }
+
+
+    public char getUserGuessInput() {
+        System.out.print("Enter letter from (a-z) or (A-Z): ");
+        String input = scanner.nextLine();
+
+        while (input.length() != 1 && !Character.isAlphabetic(input.charAt(0))) {
+            System.out.println("Invalid input. Please try again");
+            input = scanner.nextLine();
+        }
+
+        char ch = input.charAt(0);
+        if (ch >= 'A' && ch <= 'Z') {
+            ch = (char)(ch + 32);
+        }
+
+        return ch;
+    }
+
+    public void replaceFoundCharacter(char[] guessWord, String word, char ch) {
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == ch) {
+                guessWord[i] = ch;
+            }
+        }
+    }
+
+    public boolean checkUserGuessInput(String word, char input) {
+        return word.contains(Character.toString(input));
+    }
+
+    public String getRandomWord() {
+        String[] dictionary = {"government", "conversation", "condition", "preference", "decision", "daughter", "behavior",
+                "damage", "platform", "connection", "solution", "hearing", "flight", "interest", "administration",
+                "revenue", "priority", "profession", "driver"};
+
+        return dictionary[random.nextInt(dictionary.length)];
+    }
+
+}
